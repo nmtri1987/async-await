afca514883cb563e30d003e216ab90f4b43501c2
nmtri1987 <nmtri1987@gmail.com>
2018-07-31 12:29:47 +0700

    Update Parallel.cs

--- Parallel.cs
+++ Parallel.cs
@@ -1,1 +1,37 @@
 
+public class Program
+    {
+        static void Main(string[] args)
+        {
+            Go();
+        }
+        public static void Go()
+        {
+            GoAsync();
+            Console.WriteLine("*** Other Works 111 ***");
+            Console.ReadLine();
+        }
+        public static async void GoAsync()
+        {
+
+            Console.WriteLine("Starting");
+
+            var task1 = Sleep(5000);
+            var task2 = Sleep(3000);
+
+            int[] result = await Task.WhenAll(task1, task2);
+
+            Console.WriteLine("*** Other Works 222 ***");
+
+            Console.WriteLine("Slept for a total of " + result.Sum() + " ms");
+
+        }
+
+        private async static Task<int> Sleep(int ms)
+        {
+            Console.WriteLine("Sleeping for {0} at {1}", ms, DateTime.Now);
+            await Task.Delay(ms);
+            Console.WriteLine("Sleeping for {0} finished at {1}", ms, DateTime.Now);
+            return ms;
+        }
+    }
