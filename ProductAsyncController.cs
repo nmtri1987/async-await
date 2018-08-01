6b2941824b7cd693d77987756262f2fc4d04dcd8
nmtri1987 <nmtri1987@gmail.com>
2018-08-01 19:16:22 +0700

    Create ProductAsyncController.cs

---
+++ ProductAsyncController.cs
@@ -0,0 +1,37 @@
+public class ProductAsyncController : ApiController
+    {
+        Product[] products = new Product[]
+        {
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+        };
+
+        public Product DoWork(int id)
+        {
+            for (int x = 0; x <= 3; x++)
+            {
+                for (int i = 0; i != 1000000000; ++i)
+                    ;
+            }
+
+            return products.FirstOrDefault((p) => p.Id == id);
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public async Task<IHttpActionResult> GetProduct(int id)
+        {
+            var product = await Task.Run(() => DoWork(id));
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+    }
